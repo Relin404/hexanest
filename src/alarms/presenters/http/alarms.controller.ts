4954d90189e9e6cfeac136bbdcfedf1072cbb2ee
relin404 <ahmadmashaal01@gmail.com>
2024-12-29 02:04:40 +0200

    feat: follow hexagonal architecture

---
+++ src/alarms/presenters/http/alarms.controller.ts
@@ -0,0 +1,29 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { AlarmsService } from 'src/alarms/application/alarms.service';
+import { CreateAlarmCommand } from 'src/alarms/application/command/create-alarm.command';
+import { CreateAlarmDto } from 'src/alarms/presenters/http/dto/create-alarm.dto';
+
+@Controller('alarms')
+export class AlarmsController {
+  constructor(private readonly alarmsService: AlarmsService) {}
+
+  @Post()
+  create(@Body() createAlarmDto: CreateAlarmDto) {
+    return this.alarmsService.create(
+      new CreateAlarmCommand(createAlarmDto.name, createAlarmDto.severity),
+    );
+  }
+
+  @Get()
+  findAll() {
+    return this.alarmsService.findAll();
+  }
+}
