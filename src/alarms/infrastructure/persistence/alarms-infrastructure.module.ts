0e127c09fafa6916f848a4eddd3dbacabe83f5fe
relin404 <ahmadmashaal01@gmail.com>
2024-12-30 02:12:44 +0200

    feat: add ORM persistence support for alarms with repository and entity integration

--- src/alarms/infrastructure/persistence/alarms-infrastructure.module.ts
+++ src/alarms/infrastructure/persistence/alarms-infrastructure.module.ts
@@ -1,10 +1,12 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { InMemoryPersistenceModule } from 'src/alarms/infrastructure/persistence/in-memory/in-memory-persistence.module';
+import { OrmAlarmPersistenceModule } from 'src/alarms/infrastructure/persistence/orm/orm-persistence.module';
 
 @Module({})
 export class AlarmsInfrastructureModule {
   static use(driver: 'orm' | 'in-memory'): DynamicModule {
-    const persistenceModule = InMemoryPersistenceModule;
+    const persistenceModule =
+      driver === 'orm' ? OrmAlarmPersistenceModule : InMemoryPersistenceModule;
 
     return {
       module: AlarmsInfrastructureModule,
