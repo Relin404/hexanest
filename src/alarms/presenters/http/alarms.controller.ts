5a5542476ab49282a16ca3b864906453af32d0b4
relin404 <ahmadmashaal01@gmail.com>
2024-12-30 02:12:03 +0200

    feat: update AlarmsController to use async/await for service methods

--- src/alarms/presenters/http/alarms.controller.ts
+++ src/alarms/presenters/http/alarms.controller.ts
@@ -8,7 +8,7 @@ import {
   Delete,
 } from '@nestjs/common';
 import { AlarmsService } from 'src/alarms/application/alarms.service';
-import { CreateAlarmCommand } from 'src/alarms/application/command/create-alarm.command';
+import { CreateAlarmCommand } from 'src/alarms/application/commands/create-alarm.command';
 import { CreateAlarmDto } from 'src/alarms/presenters/http/dto/create-alarm.dto';
 
 @Controller('alarms')
@@ -16,14 +16,14 @@ export class AlarmsController {
   constructor(private readonly alarmsService: AlarmsService) {}
 
   @Post()
-  create(@Body() createAlarmDto: CreateAlarmDto) {
-    return this.alarmsService.create(
+  async create(@Body() createAlarmDto: CreateAlarmDto) {
+    return await this.alarmsService.create(
       new CreateAlarmCommand(createAlarmDto.name, createAlarmDto.severity),
     );
   }
 
   @Get()
-  findAll() {
-    return this.alarmsService.findAll();
+  async findAll() {
+    return await this.alarmsService.findAll();
   }
 }
