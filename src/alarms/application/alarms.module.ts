91b471216f77ddca2393f48a5878365b86071580
relin404 <ahmadmashaal01@gmail.com>
2024-12-30 02:12:31 +0200

    feat: enhance AlarmsModule to support dynamic infrastructure module integration

--- src/alarms/application/alarms.module.ts
+++ src/alarms/application/alarms.module.ts
@@ -1,10 +1,21 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module, Type } from '@nestjs/common';
 import { AlarmsService } from './alarms.service';
 import { AlarmsController } from 'src/alarms/presenters/http/alarms.controller';
 import { AlarmFactory } from 'src/alarms/domain/factories/alarm.factory';
+import { AlarmsInfrastructureModule } from 'src/alarms/infrastructure/persistence/alarms-infrastructure.module';
 
 @Module({
+  imports: [AlarmsInfrastructureModule],
   controllers: [AlarmsController],
   providers: [AlarmsService, AlarmFactory],
 })
-export class AlarmsModule {}
+export class AlarmsModule {
+  static withInfrastructure(
+    infrastructureModule: Type | DynamicModule,
+  ): DynamicModule {
+    return {
+      module: AlarmsModule,
+      imports: [infrastructureModule],
+    };
+  }
+}
