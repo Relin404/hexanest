8841f78c79a00db2a0c2b4bcb3fbc717df6613d8
relin404 <ahmadmashaal01@gmail.com>
2024-12-30 02:12:58 +0200

    feat: add dynamic module registration for application with infrastructure support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,35 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DynamicModule, Module } from '@nestjs/common';
 import { AlarmsModule } from 'src/alarms/application/alarms.module';
+import { AlarmsInfrastructureModule } from 'src/alarms/infrastructure/persistence/alarms-infrastructure.module';
+import { AppController } from 'src/app.controller';
+import { AppService } from 'src/app.service';
+import { ApplicationBootstrapOptions } from 'src/common/interfaces/application-bootstrap-options.interface';
+import { CoreModule } from 'src/core/core.module';
 
 @Module({
-  imports: [AlarmsModule],
+  imports: [CoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  /**
+   * Registers the application with the given options.
+   * @param options The options to use.
+   * @returns The module configuration.
+   * @example
+   * AppModule.register({
+   *  driver: 'orm',
+   * });
+   */
+  static register(options: ApplicationBootstrapOptions): DynamicModule {
+    return {
+      module: AppModule,
+      imports: [
+        CoreModule.forRoot(options),
+        AlarmsModule.withInfrastructure(
+          AlarmsInfrastructureModule.use(options.driver),
+        ),
+      ],
+    };
+  }
+}
