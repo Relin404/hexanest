365fbdc09a15b105eb5b960c29e30796f33b085c
relin404 <ahmadmashaal01@gmail.com>
2024-12-30 02:08:59 +0200

    feat: implement AlarmsService with AlarmRepository and AlarmFactory integration

--- src/alarms/application/alarms.service.ts
+++ src/alarms/application/alarms.service.ts
@@ -1,13 +1,25 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAlarmCommand } from 'src/alarms/application/command/create-alarm.command';
+import { CreateAlarmCommand } from 'src/alarms/application/commands/create-alarm.command';
+import { AlarmRepository } from 'src/alarms/application/ports/alarm.repository';
+import { AlarmFactory } from 'src/alarms/domain/factories/alarm.factory';
 
 @Injectable()
 export class AlarmsService {
-  create(createAlarmDto: CreateAlarmCommand) {
-    return 'This action adds a new alarm';
+  constructor(
+    private readonly alarmRepostiory: AlarmRepository,
+    private readonly alarmFactory: AlarmFactory,
+  ) {}
+
+  async create(createAlarmCommand: CreateAlarmCommand) {
+    const alarm = this.alarmFactory.create(
+      createAlarmCommand.name,
+      createAlarmCommand.severity,
+    );
+
+    return await this.alarmRepostiory.save(alarm);
   }
 
-  findAll() {
-    return `This action returns all alarms`;
+  async findAll() {
+    return await this.alarmRepostiory.findAll();
   }
 }
